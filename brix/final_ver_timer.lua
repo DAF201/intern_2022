dacd93dac3d674896844816a3c5a127fa84029af
DAF201 <56328675+DAF201@users.noreply.github.com>
2022-07-21 09:11:12 -0500

    Update final_ver_timer.lua

--- brix/final_ver_timer.lua
+++ brix/final_ver_timer.lua
@@ -5,11 +5,23 @@ time_counter = 0
 --finially work on this stupid thing
 function callback_register(name, func, interval, ...)
     if registered_function[name] == nil then
-        timed_callback_functions[#timed_callback_functions + 1] = {name, interval, func}
-        registered_function[name] = {...}
+        timed_callback_functions[#timed_callback_functions + 1] = { name, interval, func }
+        registered_function[name] = { ... }
         return true
     end
     return nil
+end
+
+function callback_unregister(function_name)
+    local counter = 1
+    for key, value in pairs(timed_callback_functions) do
+        if value[1] == function_name then
+            table.remove(timed_callback_functions, counter)
+            registered_function[function_name] = nil
+            echo('unregistered function '..function_name)
+        end
+        counter = counter + 1
+    end
 end
 
 function clock()
@@ -23,26 +35,20 @@ function clock()
             else
                 for i = 1, #timed_callback_functions, 1 do
                     local function_name = timed_callback_functions[i][1]
+                    if timed_callback_functions[i][2] == 0 then
+                        break
+                    end
                     if time_counter % timed_callback_functions[i][2] == 0 then
                         local statu, error = pcall(timed_callback_functions[i][3],
-                            unpack(registered_function[function_name]), #registered_function[function_name])
+                            unpack(registered_function[function_name], #registered_function[function_name]))
+                        -- print('executing ' .. function_name)
                         if not statu then
-                            echo("error: " .. timed_callback_functions[i][1])
-                            echo(error)
+                            print('error: ' .. timed_callback_functions[i][1])
+                            print(error)
                         end
                     end
                 end
             end
-
         end
-
-    end
-
-end
-
-function unpack(t, i)
-    i = i or 1
-    if t[i] ~= nil then
-        return t[i], unpack(t, i + 1)
     end
 end
