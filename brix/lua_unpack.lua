c74644c59a799e10b43378f1e5acc5f830d64a1c
DAF201 <56328675+DAF201@users.noreply.github.com>
2022-07-21 11:04:24 -0500

    Create lua_unpack.lua

---
+++ brix/lua_unpack.lua
@@ -0,0 +1,10 @@
+packing_counter=0
+function unpack(tab)
+    packing_counter=packing_counter+1
+    if tab[packing_counter]~=nil then
+    return tab[packing_counter],unpack(tab)
+    else
+        packing_counter=0
+    end
+end
+--still cannot handel table well
